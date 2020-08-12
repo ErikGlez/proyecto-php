7601cc0479478b59ad0d6c5eb25d66ea57ac9fdb
erikgonzalez <erik.gonzalez.ac@gmail.com>
2020-08-12 16:13:51 -0700

    Estilos del contenido principal y footer

--- index.php
+++ index.php
@@ -87,35 +87,48 @@
         <div id="principal">
             <h1>Ultimas entradas</h1>
             <article class="entrada">
+            <a href="">
                 <h2>Titulo de mi entrada</h2>
                 <p>
                     descripcion de la entrada
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia eum provident quas nostrum nobis inventore qui aut dolor esse tempora, repellat, vel, doloribus illo quis ullam nisi tempore officia id.
                 </p>
+                </a>
             </article>
             <article class="entrada">
+            <a href="">
                 <h2>Titulo de mi entrada</h2>
                 <p>
                     descripcion de la entrada
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia eum provident quas nostrum nobis inventore qui aut dolor esse tempora, repellat, vel, doloribus illo quis ullam nisi tempore officia id.
                 </p>
+                </a>
             </article>
             <article class="entrada">
+            <a href="">
                 <h2>Titulo de mi entrada</h2>
                 <p>
                     descripcion de la entrada
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia eum provident quas nostrum nobis inventore qui aut dolor esse tempora, repellat, vel, doloribus illo quis ullam nisi tempore officia id.
                 </p>
+                </a>
             </article>
             <article class="entrada">
+            <a href="">
                 <h2>Titulo de mi entrada</h2>
                 <p>
                     descripcion de la entrada
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia eum provident quas nostrum nobis inventore qui aut dolor esse tempora, repellat, vel, doloribus illo quis ullam nisi tempore officia id.
                 </p>
+                </a>
+               
             </article>
+            <div id="ver-todas">
+            <a href="">Ver todas las entradas</a>
         </div>
+        </div> <!-- fin principal -->
 
+        <div class="clearfix"></div>
     </div>
   
     <!-- pie de pagina-->
